94f1da81ddba5c74465aa053a2a0c9f79586765b
ghemsley <ghemsley@protonmail.ch>
2021-03-07 19:19:17 -0600

    commit tree 5c84967d71f7c5dd4218c93046726ceb8f88260d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,19 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new
+    @students = Student.all
+    @student_info = session[:student] if session[:student]
+  end
+
+  def create 
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
+    @student.save
+    session[:student] = params[:student]
+    redirect_to new_student_path
+  end
 
 end
